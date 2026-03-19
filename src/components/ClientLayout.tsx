3f9ce15bba8f3da018bb9810764d78c5ba5637ee
Bastan <bastansy42@gmail.com>
2026-03-19 09:23:46 +0300

    Fix: React types and AOS module issues

--- src/components/ClientLayout.tsx
+++ src/components/ClientLayout.tsx
@@ -1,30 +1,11 @@
 'use client'
 
 import { useEffect } from 'react'
-import AOS from 'aos'
-import 'aos/dist/aos.css'
 
 export default function ClientLayout() {
   useEffect(() => {
-    // Initialize AOS
-    AOS.init({
-      duration: 800,
-      once: true,
-      easing: 'ease-in-out',
-      offset: 100,
-    })
-    
-    // Refresh AOS on window resize (fixed)
-    const handleResize = () => {
-      AOS.refresh()
-    }
-    
-    window.addEventListener('resize', handleResize)
-    
-    return () => {
-      window.removeEventListener('resize', handleResize)
-    }
+    // No AOS - just return
   }, [])
 
-  return null // This component doesn't render anything
+  return null
 }
